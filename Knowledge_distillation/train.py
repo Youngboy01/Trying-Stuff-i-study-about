939aa36570bf70264045530103b8244ced2da4d8
Youngboy01 <mitukhandal01@gmail.com>
2025-09-27 16:57:40 +0530

    Contains custom trainer and basic trainer

--- Knowledge_distillation/train.py
+++ Knowledge_distillation/train.py
@@ -4,7 +4,9 @@ import torch.nn.functional as F
 from transformers import Trainer
 
 
-class ImageDistilTrainer(Trainer):
+class ImageDistillTrainer(Trainer):
+    """The main goal of this class is to train student model to mimic the soft targets of larger pretrained teacher model"""
+
     def __init__(
         self,
         teacher_model,
@@ -17,38 +19,35 @@ class ImageDistilTrainer(Trainer):
         super().__init__(model=student_model, *args, **kwargs)
         self.teacher = teacher_model
 
-        # Move the teacher model to the correct device and set to eval mode
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        device = torch.device(
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )  # moving teacher model to same device as student model
         self.teacher.to(device)
-        self.teacher.eval()
+        self.teacher.eval()  # since we dont want to train the teacher model we freeze its weights, disable dropout and normalization
 
-        self.loss_function = nn.KLDivLoss(reduction="batchmean")
+        self.loss_function = nn.KLDivLoss(reduction="batchmean")  # KLD loss
         self.temperature = temperature
-        self.lambda_param = lambda_param
+        self.lambda_param = lambda_param  # overall loss is weighted sum of KLD loss and CEloss so lmbda controls the weights
 
     def compute_loss(self, model, inputs, return_outputs=False):
-        # We pass the inputs to both student and teacher models
         student_output = model(**inputs)
 
         with torch.no_grad():
             teacher_output = self.teacher(**inputs)
 
-        # Compute soft targets for teacher and student
-        # The teacher's distribution is normalized using softmax
+        # Computing soft targets for teacher and student
         soft_teacher = F.softmax(teacher_output.logits / self.temperature, dim=-1)
-        # The student's distribution is normalized using log_softmax
-        soft_student = F.log_softmax(student_output.logits / self.temperature, dim=-1)
+        soft_student = F.log_softmax(
+            student_output.logits / self.temperature, dim=-1
+        )  # log softmax cause nn.KLDloss expects input in log probability space
 
-        # Compute the distillation loss (KL Divergence)
-        # We scale the loss by the square of the temperature as recommended by Hinton et. al
-        distillation_loss = self.loss_function(soft_student, soft_teacher) * (
-            self.temperature**2
-        )
+        distillation_loss = (
+            self.loss_function(soft_student, soft_teacher) * (self.temperature**2)
+        )  # The T**2 scaling is essential because when the temperature T is high, the gradients produced by the soft targets are smaller. Scaling by T**2 ensures the distillation loss does not become insignificant compared to the standard loss.
 
-        # Compute the student's own loss on the true labels
-        student_target_loss = student_output.loss
+        student_target_loss = student_output.loss  # this computes the CEloss
 
-        # Calculate the final loss as a weighted sum of the two loss components
+        # Calculating the final loss as a weighted sum of the two loss components
         loss = (
             1.0 - self.lambda_param
         ) * student_target_loss + self.lambda_param * distillation_loss
